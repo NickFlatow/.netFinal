2e61e30762bb61d8f302391ea19e477dff9a2f94
NickFlatow <nick.flatow@gmail.com>
2019-04-22 18:33:15 -0500

    started discount edit

---
+++ Controllers/DiscountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class DiscountController : Controller
+    {
+        private INorthwindRepository repository;
+        public DiscountController(INorthwindRepository repo) => repository = repo;
+
+        public IActionResult Index() => View(repository.Discounts.FirstOrDefault(d => d.DiscountID == 1));
+
+    }
+}
--- Models/Discount.cs
+++ Models/Discount.cs
@@ -11,7 +11,6 @@ namespace Northwind.Models
         public decimal DiscountPercent { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-
         public Product Product { get; set; }
     }
 }

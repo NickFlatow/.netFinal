b197bf09052e174ac9c8ce7dfb916c819e1c40d8
Krishnapriya Krishnamoorthy <kkrishnamoorthy@my.wctc.edu>
2019-05-09 13:05:01 -0500

    Added Validation to Discout add page

--- Models/Discount.cs
+++ Models/Discount.cs
@@ -6,15 +6,18 @@ namespace Northwind.Models
     public class Discount
     {
         public int DiscountID { get; set; }
+        [Required(ErrorMessage = "Code is required")]
         public int Code { get; set; }
-        [Range(typeof(DateTime), "5/01/2019", "06/08/2019")]
+        [Required(ErrorMessage = "Start time is required")]
         public DateTime StartTime { get; set; }
-        [Range(typeof(DateTime), "5/01/2019", "06/08/2019")]
+        [Required(ErrorMessage = "End Time is required")]
         public DateTime EndTime { get; set; }
         public int ProductID { get; set; }
         public decimal DiscountPercent { get; set; }
+        [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; }
         public string Description { get; set; }
+        [Required(ErrorMessage = "Product is required")]
         public Product Product { get; set; }
     }
 }
